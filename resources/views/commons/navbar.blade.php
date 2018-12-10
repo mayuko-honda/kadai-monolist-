d5cb12d5aad55a9b258d155e6daca8031ea5ff6d
mayuko.honda <hiro.saya.kouchan@gmail.com>
2018-12-10 03:45:59 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -14,6 +14,8 @@
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="#">新規登録</a></li>
                     <li><a href="#">ログイン</a></li>
+                    <li><a href="{{ route('signup.get') }}">新規登録</a></li>
+
                 </ul>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 Route::get('/', 'WelcomeController@index');
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 });
