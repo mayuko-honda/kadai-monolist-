3525170c421a30afaf2fa97cf8a36bdbcadbd875
mayuko.honda <hiro.saya.kouchan@gmail.com>
2018-12-14 12:34:19 +0000

    ranking

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -48,6 +48,44 @@
                     @endif
                 </ul>
             </div>
+            @if (Auth::check())
+                        <li>
+                            <a href="#">
+                                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                                アイテムを追加
+                              </a>
+                        </li>
+
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                <span class="glyphicon glyphicon-signal" aria-hidden="true"></span>
+                                ランキング
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="{{ route('ranking.want') }}">Wantランキング</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                <span class="gravatar">
+                                    <img src="{{ Gravatar::src(Auth::user()->email, 20) . '&d=mm' }}" alt="" class="img-circle">
+                                </span>
+                                {{ Auth::user()->name }}
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a href="{{ route('users.show', Auth::id()) }}">マイページ</a>
+                                </li>
+                                <li role="separator" class="divider"></li>
+                                <li>
+                                    <a href="{{ route('logout.get') }}">ログアウト</a>
+                                </li>
+                            </ul>
+                        </li>
+                    @else
         </div>
     </nav>
 </header>
